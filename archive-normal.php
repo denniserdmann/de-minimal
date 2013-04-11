8929c0bb4235439a00c5c1a605ce0e191b530e4f
Dennis Erdmann <denniserdmann@dennismb.fritz.box>
2013-04-11 07:57:45 +0200

    cleaned up code, deleted unnecessary lines

--- archive-normal.php
+++ archive-normal.php
@@ -10,50 +10,44 @@ get_header(); ?>
  <?php echo category_description( get_category_by_slug('category-slug')->term_id ); ?> 
  </div>
  <?php endif; ?>
- 
-		<?php if (have_posts()) : ?>
-		<?php $count = 1 ?>
-		<?php while(have_posts()) : the_post(); ?>
-		<?php $einruecken = get_post_meta($post->ID, "standard", true); ?>
-		<article id="post-<?php the_ID(); ?>" class="post <?php if (($einruecken=="") and (in_category( 'magazine' ))) echo 'nopadding'; ?>" >
-			<div class="background">
-			  <div class="entry<?php if (in_category( 'magazine' )) echo " magstyle"; ?>">
-								
-<?php if (is_linked_list()): ?>
-  <h2 class="<?php the_ID(); ?> linktipp"><a style="color: #B33333" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">&#9733;</a> <a href="<?php the_linked_list_link(); ?>" title="<?php printf( esc_attr__( 'Link to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"> <?php the_title(); ?></a></h2>
-  
-	<?php else: ?>
+<?php if (have_posts()) : ?>
+<?php $count = 1 ?>
+<?php while(have_posts()) : the_post(); ?>
+<?php $einruecken = get_post_meta($post->ID, "standard", true); ?>
+<article id="post-<?php the_ID(); ?>" class="post <?php if (($einruecken=="") and (in_category( 'magazine' ))) echo 'nopadding'; ?>" >
+	<div class="background">
+	<div class="entry">
+	<?php if (is_linked_list()): ?>
+		<h2 class="<?php the_ID(); ?> linktipp">
+			<a style="color: #B33333" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">&#9733;</a>
+			<a href="<?php the_linked_list_link(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
+		</h2>
+<?php else: ?>
 		<h2 class="<?php the_ID(); ?>">
-		    <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
-		        <?php the_title(); ?>
-		    </a>
+			<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
 		</h2>
 	<?php endif; ?>
-	
-		<aside class="sidebar"><div class="date"><i class="icon-calendar"></i> <?php the_time('j. F Y') ?></div>
-		<?php if (get_comments_number()>=1 ) : ?>
-			  <div class="commentcount">
-			  	<i class="icon-comments"></i> <?php comments_popup_link(__('Kein Kommentar'), __('1 Kommentar'), __('% Kommentare'), __('') ); ?>
-			  </div>
-			  <?php endif; ?>
+		<aside class="sidebar">
+			<div class="date"><i class="icon-calendar"></i> <?php the_time('j. F Y') ?></div>
+			<?php // if comments exists ?>
+			<?php if (get_comments_number()>=1 ) : ?>
+			<div class="commentcount">
+				<i class="icon-comments"></i> <?php comments_popup_link(__('Kein Kommentar'), __('1 Kommentar'), __('% Kommentare'), __('') ); ?>
+			</div>
+			<?php endif; ?>
 			 <?php the_tags('<div class="tags">Tags: ',',  ','</div>');  ?>  
 		</aside>
-
-        <?php the_content('Lies den Rest des Artikels &raquo;'); ?>
-                     <?php if (is_linked_list()): ?>
-				<p><a class="button" href="<?php the_linked_list_link(); ?>" title="<?php printf( esc_attr__( 'Weiter zum Artikel: %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>">
+		<?php the_content('Lies den Rest des Artikels &raquo;'); ?>
+			<?php if (is_linked_list()): ?>
+				<p><a class="button" href="<?php the_linked_list_link(); ?>" title="<?php the_title_attribute(); ?>">
 					Artikel ansehen
 				</a></p>
 				<?php endif; ?>
-                    </div><!-- Ende entry -->
-                     <?php if ($count == 4) wp_pagenavi(); ?>
-			</div> <!-- neues Ende background  -->
-	
-		</article> <!-- ende id artikel -->
-            <?php //} ?>
-            
+	</div><!-- Ende entry -->
+	<?php if ($count == 4) wp_pagenavi(); ?>
+	</div> <!-- neues Ende background  -->
+</article> <!-- ende id artikel -->
 <?php $count++ ?>
-
 		<?php endwhile; ?>
 	<?php else :
 
