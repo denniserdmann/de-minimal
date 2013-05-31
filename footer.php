ce5235c23fc46802a16912fee68a3f5e9e448335
Dennis Erdmann <denniserdmann@dennis.fritz.box>
2013-05-31 08:58:42 +0200

    deleted Google Analytics Code and used a plugin instead. My choice: „Google Analyticator“

--- footer.php
+++ footer.php
@@ -9,16 +9,5 @@
 		<?php wp_nav_menu( array('menu_class' => 'nav_footer entry' )); ?>
 			
 </footer>
-<?php if(!is_user_logged_in()): ?>
-<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>
-<script type="text/javascript">
-	<!--//--><![CDATA[//><!--
-	var pageTracker = _gat._getTracker("UA-4540826-2");
-	pageTracker._initData();
-	_gat._anonymizeIp();
-	pageTracker._trackPageview();
-	//--><!]]>
-</script>
-<?php endif; ?>
 </body>
 </html>
