cac1cc6d18bb9c5bb094988a12a399ebb90ff416
Dennis Erdmann <dennis.erdmann@solades.de>
2013-08-11 13:49:59 +0200

    changed navigation: added rss and search and styled it

--- js/master.js
+++ js/master.js
@@ -13,5 +13,22 @@ jQuery(function($) {
 
     $('#offCanvasToggler').on('click', toggleOnCanvas);
     $('nav').on('click', toggleOffCanvas);
-    $('.searchform').on('click', toggleOffCanvas);
+    //$('.searchform').on('click', toggleOffCanvas);
+});
+
+/**
+ * Search Form
+ */
+ 
+ jQuery(function($) {
+    var toggleOnSearch = function(e) {
+        e.preventDefault();
+        $('body').toggleClass('offSearch');
+    };
+    
+    var toggleOffSearch = function(e) {
+        $('body').toggleClass('offSearch');
+    };
+
+    $('#searchToggler').on('click', toggleOnSearch);
 });
