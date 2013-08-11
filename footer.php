2ae7e5ab7e5f5e4f1f0399d87a8b4588ef5bab60
Dennis Erdmann <dennis.erdmann@solades.de>
2013-08-11 16:00:02 +0200

    created new design for footer + footer navigation

--- footer.php
+++ footer.php
@@ -7,6 +7,10 @@
 <footer>
 		<?php wp_footer(); ?>
 		<?php wp_nav_menu( array('menu_class' => 'nav_footer entry' )); ?>
+		<p>
+			<a href="https://github.com/denniserdmann/de-minimal" title="Weiter zum Github Projekt">Wordpress-Theme auf Github</a><br>
+			&copy; 2013 Dennis Erdmann, Kiel
+		</p>
 			
 </footer>
 </body>
