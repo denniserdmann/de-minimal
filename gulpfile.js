926d13be8bcc36f3a4895dc3cdc655cfddb3dd30
Dennis Erdmann <dennis.erdmann@solades.de>
2014-06-21 14:26:03 +0200

    duplicated and renamed less files to scss

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp        = require('gulp');
+
+var sass 		= require('gulp-sass'),
+    livereload  = require('gulp-livereload'),
+    lr          = require('tiny-lr'),
+    server      = lr();
+
+var paths = {
+    master: {
+        styles: 'scss/style.scss',
+    },
+    watch: {
+        styles: 'scss/**/*.scss',
+    },
+    dist: {
+        styles: 'css',
+    }
+};
+
+gulp.task('styles', function () {
+    gulp.src(paths.master.styles)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.dist.styles))
+        .pipe(livereload(server));
+});
+
+gulp.task('watch', function() {
+    server.listen(8000, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        gulp.watch(paths.watch.styles, ['styles']);
+    });
+});
+
+gulp.task('default', ['styles', 'watch']);
